2d4a40b648f81cdc15a63032d497853f578c96c9
Senthil S <ssthil@gmail.com>
2026-06-13 19:49:24 +0800

    feat(taskbaton): bump formula to v0.2.0

--- Formula/taskbaton.rb
+++ Formula/taskbaton.rb
@@ -1,28 +1,28 @@
 class Taskbaton < Formula
   desc "Pass work state between agentic tool sessions"
   homepage "https://github.com/ssthil/taskbaton"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/ssthil/taskbaton/releases/download/v0.1.0/taskbaton_0.1.0_darwin_arm64.tar.gz"
-      sha256 "ca0a823c6da5e764331fb229681f4d9f521336df8224f7b57bc2ec5810a4afd7"
+      url "https://github.com/ssthil/taskbaton/releases/download/v0.2.0/taskbaton_0.2.0_darwin_arm64.tar.gz"
+      sha256 "d03cfd467f6d914d34c48cca2e17434d43a9ecc662fcdbe68f6e31609c2a80d4"
     end
     on_intel do
-      url "https://github.com/ssthil/taskbaton/releases/download/v0.1.0/taskbaton_0.1.0_darwin_amd64.tar.gz"
-      sha256 "592f9650684c2630b4a19fedf3d13ec3b2127a0ac409019db3d831c074ae9ebf"
+      url "https://github.com/ssthil/taskbaton/releases/download/v0.2.0/taskbaton_0.2.0_darwin_amd64.tar.gz"
+      sha256 "3f4b03c469a3d1e5c537493fd852286b3c966897cb0b0289ea6d93487b15f042"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/ssthil/taskbaton/releases/download/v0.1.0/taskbaton_0.1.0_linux_arm64.tar.gz"
-      sha256 "f95c2bfb9f28096499ac6558c2a5f52ad3e2f2f35834b5ed955665e2fd6e5b04"
+      url "https://github.com/ssthil/taskbaton/releases/download/v0.2.0/taskbaton_0.2.0_linux_arm64.tar.gz"
+      sha256 "b06326fdf7945095d8d6f02d8c8351667afb12eacc4587b93b59d06526467d31"
     end
     on_intel do
-      url "https://github.com/ssthil/taskbaton/releases/download/v0.1.0/taskbaton_0.1.0_linux_amd64.tar.gz"
-      sha256 "2302591ee3ed3eebb35a066ab9fd8a6417e85acab8dad8106e40989e0bfe136e"
+      url "https://github.com/ssthil/taskbaton/releases/download/v0.2.0/taskbaton_0.2.0_linux_amd64.tar.gz"
+      sha256 "65c26d7a050a3cd09a9a2ad96d3a8ee1ce24ee7fd8ee37465587c92b127eaa0e"
     end
   end
 
